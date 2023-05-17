c47a9c87b86d664fb0fa9919df33c1384e521579
marc-antoine ourradour <ourradourm@gmail.com>
2023-05-17 10:22:50 +0200

    save

--- mint_Webiste/Frontend/src/pages/MyDivergent/MyDivergent.tsx
+++ mint_Webiste/Frontend/src/pages/MyDivergent/MyDivergent.tsx
@@ -10,6 +10,7 @@ import { ArrowUpRightIcon } from "@heroicons/react/24/solid";
 import { divg } from "../../assets";
 import Card from "./Card/Card";
 import { reveal } from "../../utils/ethers/ethers.utils";
+import ItemPreview from "./ItemPreview/ItemPreview";
 
 const MyDivergent = () => {
   const [selectedCapsule, setSelectedCapsule] = useState<Capsule | undefined>();
@@ -34,11 +35,13 @@ const MyDivergent = () => {
   };
 
   return (
-    <div className={` flex justify-center bg-capsule  `}>
+    <div className={` bg-capsule flex justify-center  `}>
       {hasNFT ? (
         <>
           <div className="flex flex-col">
-            <p className="text-[40px] font-medium ml-10">Owned</p>
+            {!selectedCapsule && (
+              <p className="ml-10 text-[40px] font-medium">Owned</p>
+            )}
             <div className="flex flex-wrap items-center ">
               {!selectedCapsule &&
                 capsulesDatas.map((capsule: Capsule, index) => (
@@ -53,59 +56,17 @@ const MyDivergent = () => {
 
               {!selectedCapsule &&
                 characters_silver.map((character: Character, index) => (
-                  <Card character={character} onClick={() => {}} />
+                  <Card character={character} onClick={() => {}} key={index} />
                 ))}
             </div>
           </div>
           {selectedCapsule && (
-            <div>
-              <div className="flex flex-col items-center ">
-                <div
-                  className="flex flex-col lg:flex-row justify-between  h-full w-auto lg:h-auto 
-                      bg-white bg-opacity-25 rounded-xl transform"
-                >
-                  <div className="flex items-center justify-center w-full lg:w-auto">
-                    <img
-                      src={selectedCapsule.imageBG}
-                      alt={selectedCapsule.title}
-                      className="w-full h-auto object-cover lg:w-auto"
-                    />
-                  </div>
-                  <div className="w-full lg:w-1/2 flex flex-col justify-between p-4 lg:pl-8 ">
-                    <button
-                      onClick={() => handleViewClick(undefined)}
-                      className="self-start"
-                    >
-                      Close
-                    </button>
-                    <div>
-                      <div className="flex flex-col h-full justify-between mt-auto items-center">
-                        <h2 className="font-bold text-2xl">
-                          {selectedCapsule.description}
-                        </h2>
-                        <div className="selected-capsule-buttons space-x-2 mt-8">
-                          <button
-                            className="capsule-button put-on-sale-button text-black font-bold"
-                            onClick={() =>
-                              window.open("https://opensea.io/", "_blank")
-                            }
-                          >
-                            Put on sale
-                          </button>
-                          <button
-                            className="capsule-button put-on-sale-button text-black font-bold"
-                            onClick={reveal}
-                          >
-                            Reveal
-                          </button>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <Droprate CharacterData={selectedCapsule.character} />
-            </div>
+            <ItemPreview
+              selectedCapsule={selectedCapsule}
+              onClick={() => {
+                handleViewClick(undefined);
+              }}
+            />
           )}
         </>
       ) : (
