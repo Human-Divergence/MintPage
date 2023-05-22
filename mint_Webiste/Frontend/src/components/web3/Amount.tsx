66685052da7e384b94a5ef508d2762eb29e23dad
Slpn <shaan99@live.fr>
2023-05-22 14:35:47 +0200

    Checkout (need to fixed at the right)

--- mint_Webiste/Frontend/src/components/web3/Amount.tsx
+++ mint_Webiste/Frontend/src/components/web3/Amount.tsx
@@ -11,55 +11,61 @@ const Amount = () => {
   return (
     <>
       <div className="relative">
-        <div className="z-10 bg-opacity-45 border border-black">
-          <div className="grid grid-cols-2 items-center w-[312px]">
+        <div className="bg-opacity-45 z-10 border border-black">
+          <div className="grid w-[312px] grid-cols-2 items-center">
             <span className="text-[14px] font-bold">Capsules</span>
-            <span className="text-[14px] font-bold ml-8">Quantity</span>
+            <span className="ml-8 text-[14px] font-bold">Quantity</span>
             <div className="flex items-center">
-              <span className="text-[24px] text-[#999999] font-bold">QUELOZ ONYX</span>
+              <span className="text-[24px] font-bold text-[#999999]">
+                QUELOZ ONYX
+              </span>
             </div>
             <div className="flex items-center justify-center">
               <span className="text-[40px]">x X</span>
             </div>
             <div className="flex items-center">
-              <span className="text-[24px] text-[#AD7000] font-bold">MELT GOLD</span>
+              <span className="text-[24px] font-bold text-[#AD7000]">
+                MELT GOLD
+              </span>
             </div>
             <div className="flex items-center justify-center">
               <span className="text-[40px]">x X</span>
             </div>
             <div className="flex items-center">
-              <span className="text-[24px] text-[#2B1E74] font-bold">TRIA DIAMOND</span>
+              <span className="text-[24px] font-bold text-[#2B1E74]">
+                TRIA DIAMOND
+              </span>
             </div>
             <div className="flex items-center justify-center">
               <span className="text-[40px]">x X</span>
             </div>
           </div>
         </div>
-      <div className=" bg-opacity-45 border border-black w-[312px]">
-        <div className="flex items-start"> 
-          <span className="text-[24px] font-bold">Total</span>
+        <div className=" bg-opacity-45 w-[312px] border border-black">
+          <div className="flex items-start">
+            <span className="text-[24px] font-bold">Total</span>
+          </div>
+          <div className="flex items-center justify-end">
+            <img src={eth} alt="ETH Logo" className="mr-2" />
+            <span className="text-2xl font-bold">{totalAmount} ETH</span>
+          </div>
+          <div className="mt-2 flex items-center justify-end">
+            <span className="text-xl font-bold text-[#999999]">XXXX USD</span>
+          </div>
+          <div className="flex items-start"> 
+            <span className="text-[24px] font-bold">Fees</span>
+          </div>
+          <div className="flex items-center justify-end">
+            <img src={eth} alt="ETH Logo" className="mr-2" />
+            <span className="text-2xl font-bold">{totalAmount} ETH</span>
+          </div>
+          <div className="mt-2 flex items-center justify-end">
+            <span className="text-xl font-bold text-[#999999]">XXXX USD</span>
+          </div>
+          <div className="mt-2 flex justify-center">
+            <img src={Purchase} alt="Purchase" className="w-[270px]" />
+          </div>
         </div>
-        <div className="flex items-center justify-end">
-          <img src={eth} alt="ETH Logo" className="mr-2" />
-          <span className="text-2xl font-bold">{totalAmount} ETH</span>
-        </div>
-        <div className="flex items-center justify-end mt-2">
-          <span className="text-xl text-[#999999] font-bold">XXXX USD</span>
-        </div>
-        <div className="flex items-start"> 
-          <span className="text-[24px] font-bold">Fees</span>
-        </div>
-        <div className="flex items-center justify-end">
-          <img src={eth} alt="ETH Logo" className="mr-2" />
-          <span className="text-2xl font-bold">{totalAmount} ETH</span>
-        </div>
-        <div className="flex items-center justify-end mt-2">
-          <span className="text-xl text-[#999999] font-bold">XXXX USD</span>
-        </div>
-        <div className="flex justify-center mt-2">
-        <img src={Purchase} alt="Purchase" className="w-[270px]" />
-      </div>
-      </div>
       </div>
     </>
   );
